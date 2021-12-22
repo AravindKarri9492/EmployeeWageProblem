14f719f2fa0fff99c54980f5a3d7f9ea9a265cd6
AravindKarri9492 <n140035@rguktn.ac.in>
2021-12-22 22:31:17 +0530

    ADDED: compute daily wage of an employee

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,12 +4,17 @@ public class EmployeeWage
 {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Employee Wage Computation Program");
         int IS_EMP_PRESENT = 1;
+        int EMP_HRS = 8;
+        int EMP_RATE_PER_HR = 20;
+        int dailyWage = 0;
+        System.out.println("Welcome to Employee Computation Program on Master Branch");
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_EMP_PRESENT)
         {
             System.out.println("Employee is present");
+            dailyWage = (EMP_HRS * EMP_RATE_PER_HR);
+            System.out.println("Employee daily wage is: " +dailyWage);
         }
         else
         {
