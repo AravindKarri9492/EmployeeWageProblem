9177c2963f2131ad23412e2e95896579ab6c8c81
AravindKarri9492 <n140035@rguktn.ac.in>
2021-12-24 21:16:34 +0530

    REFACTORED: random numbers logic developed

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,9 +5,9 @@ public class EmployeeWage
     //constants
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static final int EMP_RATE_PER_HR = 20;
+    public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int MAX_WORKING_HRS = 100;
+    public static final int MAX_WORKING_HOURS = 100;
 
     public static void main(String args[])
     {
@@ -18,10 +18,10 @@ public class EmployeeWage
         int totalEmpHrs = 0;
         System.out.println("Welcome to Employee Computation Program");
 
-        while (totalWorkingDays < NUM_OF_WORKING_DAYS && totalEmpHrs <MAX_WORKING_HRS)
+        while (totalWorkingDays < NUM_OF_WORKING_DAYS && totalEmpHrs <MAX_WORKING_HOURS)
         {
             totalWorkingDays++;
-            double empCheck = (Math.floor(Math.random() * 10) % 2) + 1;
+            double empCheck = (Math.floor(Math.random() * 10) % 3);
             switch ((int) empCheck)
             {
                 case IS_FULL_TIME:
@@ -37,7 +37,7 @@ public class EmployeeWage
             }
             totalEmpHrs += empHrs;
         }
-        salary = (totalEmpHrs * EMP_RATE_PER_HR);
+        salary = (totalEmpHrs * EMP_RATE_PER_HOUR);
         System.out.println("Total working days "+totalWorkingDays );
         System.out.println("Total working hrs " +totalEmpHrs);
         System.out.println("Employee Wage for a month is " + salary);
